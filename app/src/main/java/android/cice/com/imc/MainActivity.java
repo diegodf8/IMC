b4297209bdc85d1e45c83ab9ba319d8787a148f1
diegodf8 <diegodf8@gmail.com>
2018-05-09 23:12:48 +0200

    Android objetos

--- app/src/main/java/android/cice/com/imc/MainActivity.java
+++ app/src/main/java/android/cice/com/imc/MainActivity.java
@@ -2,6 +2,9 @@ package android.cice.com.imc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText altura = findViewById(R.id.altura);
+        EditText peso = findViewById(R.id.peso);
+        Button calcula = findViewById(R.id.calcula);
+        TextView resultado = findViewById(R.id.resultado);
+        
+
     }
 }
