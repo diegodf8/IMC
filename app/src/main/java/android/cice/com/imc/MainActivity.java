b3ecaea7abf2eeb1a5613e70a8d41b5dd51b0ce2
diegodf8 <diegodf8@gmail.com>
2018-05-20 12:34:29 +0200

    Android IMC con listas

--- app/src/main/java/android/cice/com/imc/MainActivity.java
+++ app/src/main/java/android/cice/com/imc/MainActivity.java
@@ -9,12 +9,22 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 
         final EditText altura = findViewById(R.id.altura);
         final EditText peso = findViewById(R.id.peso);
